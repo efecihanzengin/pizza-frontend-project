3e3fe7e26903bc3198b50ad594f8f617d2816e04
Efe Cihan Zengin <112198832+efecihanzengin@users.noreply.github.com>
2024-12-31 15:07:19 +0300

    homepage styling done
    - iteration 1 homepage styling done

---
+++ src/components/HomeButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/Homepage.css";
+
+function HomeButton() {
+  return (
+    <button className="homebutton">ACIKTIM</button>
+  );
+}
+
+export default HomeButton;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../css/Homepage.css";
+import HomeButton from "../components/HomeButton";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="homepage-text">
+        <h1>Teknolojik Yemekler</h1>
+        <h2>KOD ACIKTIRIR, PIZZA DOYURUR</h2>
+        {/* Add a button here */}
+        <HomeButton />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
